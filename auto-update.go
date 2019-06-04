c15db3a5bd0d0296fadb58d9f7538bce5c52d716
Corebreaker <frederic.meyer.77@gmail.com>
2019-06-04 07:20:26 +0300

    Only update changed files

--- auto-update.go
+++ auto-update.go
@@ -3,20 +3,35 @@ package main
 import (
 	"os"
 	"path/filepath"
+	"time"
 
 	gerr "github.com/corebreaker/goerrors"
 	"github.com/siddontang/go/ioutil2"
 	"github.com/tywkeene/go-fsevents"
 )
 
-type tFileEventHandler string
+type tFileEventHandler struct {
+	configPath string
+	lastUpdate time.Time
+}
 
-func (h tFileEventHandler) Handle(w *fsevents.Watcher, event *fsevents.FsEvent) error {
+func (h *tFileEventHandler) Handle(w *fsevents.Watcher, event *fsevents.FsEvent) error {
+	infos, err := os.Stat(event.Path)
+	if err != nil {
+		return gerr.DecorateError(err)
+	}
+
+	if (!h.lastUpdate.IsZero()) && (h.lastUpdate.Equal(infos.ModTime())) {
+		return nil
+	}
+
+	h.lastUpdate = infos.ModTime()
+
 	return UpdateConfig(event.Path)
 }
 
 func (h tFileEventHandler) Check(event *fsevents.FsEvent) bool {
-	return event.Path == string(h)
+	return event.Path == h.configPath
 }
 
 func (h tFileEventHandler) GetMask() uint32 {
@@ -30,27 +45,27 @@ func (tFallbackHandler) Check(event *fsevents.FsEvent) bool                     
 func (tFallbackHandler) GetMask() uint32                                           { return fsevents.AllEvents }
 
 func InitAutoUpdate() error {
-	confPath, err := filepath.Abs(os.Args[1])
+	configPath, err := filepath.Abs(os.Args[1])
 	if err != nil {
 		return gerr.DecorateError(err)
 	}
 
-	ext := filepath.Ext(confPath)
+	ext := filepath.Ext(configPath)
 	switch ext {
 	case ".json", ".yml", ".yaml":
 	default:
 		return gerr.MakeError("The config file must be in JSON or YAML format with the good file extension")
 	}
 
-	if ioutil2.FileExists(confPath) {
-		if err := UpdateConfig(confPath); err != nil {
+	if ioutil2.FileExists(configPath) {
+		if err := UpdateConfig(configPath); err != nil {
 			return err
 		}
 	}
 
 	mask := fsevents.FileChangedEvent | fsevents.FileCreatedEvent
 
-	confDir := filepath.Dir(confPath)
+	confDir := filepath.Dir(configPath)
 	if !ioutil2.FileExists(confDir) {
 		mask |= fsevents.DirCreatedEvent
 	}
@@ -60,7 +75,7 @@ func InitAutoUpdate() error {
 		return gerr.DecorateError(err)
 	}
 
-	if err := w.RegisterEventHandler(tFileEventHandler(confPath)); err != nil {
+	if err := w.RegisterEventHandler(&tFileEventHandler{configPath: configPath}); err != nil {
 		return gerr.DecorateError(err)
 	}
 
